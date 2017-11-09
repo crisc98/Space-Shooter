b458788162ccc71ef0f862893e0e41f1a2240acb
Cristiano Chelotti <Cristiano.Chelotti@capitalone.com>
2017-11-09 16:48:23 -0500

    Adding background scroll and VFX + enemy ship prefab

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,66 +3,81 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
-    public Vector3 spawnValues;
-    public int hazardCount;
-    public float spawnWait;
-    public float startWait;
-    public float waveWait;
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait; 
 
 	public GUIText scoreText;
-	public GUIText restartText, gameOverText;
-	private bool restart, gameOver;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
 	private int score;
 
-    void Start ()
-    {
-        StartCoroutine (SpawnWaves ());
-		score = 0;
+	void Start ()
+	{
 		gameOver = false;
 		restart = false;
 		restartText.text = "";
 		gameOverText.text = "";
-    }
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
 
-	void Update(){
-		if (restart && Input.GetKeyDown(KeyCode.R)) {
-			Application.LoadLevel(Application.loadedLevel);
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
 		}
 	}
 
-    IEnumerator SpawnWaves ()
-    {
-        yield return new WaitForSeconds (startWait);
-        while (true)
-        {
-            for (int i = 0; i < hazardCount; i++)
-            {
-                Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-                Quaternion spawnRotation = Quaternion.identity;
-                Instantiate (hazard, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds (spawnWait);
-            }
-            yield return new WaitForSeconds (waveWait);
-			if (gameOver) {
-				restartText.text = "Press 'R' to Restart";
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				GameObject hazard = hazards[Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
 				restart = true;
 				break;
 			}
-        }
-    }
-	void UpdateScore(){
-		scoreText.text = "Score: " + score;
+		}
 	}
 
-	public void AddScore(int newScoreValue){
+	public void AddScore (int newScoreValue)
+	{
 		score += newScoreValue;
 		UpdateScore ();
 	}
 
-	public void GameOver(){
-		gameOverText.text = "Game Over";
-		gameOver = true;
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
 	}
 
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
 }
